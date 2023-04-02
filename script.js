231ffb4ae67a087ae3cba08df9933f56ad4e60e0
bzfscript <129647762+bzfscript@users.noreply.github.com>
2023-04-02 14:46:22 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const body = document.querySelector("body"),
+	  nav = document.querySelector("nav"),
+ 	  modeToggle = document.querySelector("dark-light"),
+	  searchToggle = document.querySelector("searchBox"),
+
+	  
+	   modeToggle.addEventListener("click", () =>{
+	   modeToggle.classList.toggle("active");
